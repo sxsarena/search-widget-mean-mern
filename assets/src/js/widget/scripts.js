d02224af33a33c230ddde61396e78a4d0b313859
Hemerson Vianna <hemerson.lourenco@gmail.com>
2015-04-26 06:38:36 -0300

    javascript de validacao do formulario

--- assets/src/js/widget/scripts.js
+++ assets/src/js/widget/scripts.js
@@ -7,13 +7,70 @@ $('.js-date').pickadate({
 });
 
 //
-var elemDate = $('.js-date');
-$('.js-disabledate').on('click', function(){
-	var elem = $(this);
+var $elemDate = $('.js-date');
+var elemClassError = 'error-field';
+var messageError = "Campo obrigatório";
 
-	if( elem.is(':checked') ){
-		elemDate.prop('disabled', true);
+$('.js-disabledate').on('click', function() {
+	var $elem = $(this);
+
+	if( $elem.is(':checked') ){
+		$elemDate.prop('disabled', true);
+		$elemDate.val('');
+
+		$elemDate.each(function() {
+			var $elem = $(this);
+			if( $elem.hasClass(elemClassError) ) {
+				var $elemWrap = $($elem.closest('.error'));
+				$elem.removeClass(elemClassError);
+				$elemWrap.find('.error-msg').remove();
+				$elem.unwrap('.error');
+			}
+		});
 	} else {
-		elemDate.prop('disabled', false);
+		$elemDate.prop('disabled', false);
 	}
+});
+
+//
+var validation = function() {};
+
+validation.prototype.init = function (element) {
+	var $elem = $(element);
+
+	if( $elem.val() === '' ){
+
+		messageError = $elem.data('msg-require') || messageError;
+
+			$elem.addClass(elemClassError);
+			$($elem).wrap( "<span class='error'></span>" );
+			$("<span class='error-msg'>"+messageError+"</span>").insertAfter($elem);
+
+	} else {
+
+		if( $elem.hasClass(elemClassError) ) {
+			var $elemWrap = $($elem.closest('.error'));
+			$elem.removeClass(elemClassError);
+			$elemWrap.find('.error-msg').remove();
+			$elem.unwrap('.error');
+		}
+
+	}
+};
+
+
+$(document).on("keydown keyup", ".js-field-string", function() {
+    $(this).val( $(this).val().replace(/[^a-zA-ZãâÃÂáÁàÀêÊéÉèÈíÍìÌôÔõÕóÓòÒúÚùÙûÛçÇ ]/g, "") );
+});
+
+$('.form').on('submit', function(event) {
+	event.preventDefault();
+	$('.form-field:enabled').each(function() {
+		new validation().init(this);
+	});
+});
+
+$('.form-field:enabled').on('keyup change', function(event) {
+	event.preventDefault();
+	new validation().init(this);
 });
